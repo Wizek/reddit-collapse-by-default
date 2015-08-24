8e735314fbc34d90081f1cece909e3b793cb31b4
Milán Nagy <123.wizek@gmail.com>
2015-08-25 00:08:39 +0200

    Still show voting arrows

--- reddit-collapse-by-default.user.js
+++ reddit-collapse-by-default.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit collapse by default
 // @namespace    http://github.com/Wizek/
-// @version      0.2
+// @version      0.2.1
 // @description  All Reddit comments collapsed by default
 // @author       Milan Nagy
 // @match        https://www.reddit.com/*
@@ -11,6 +11,10 @@
 GM_addStyle("                     \
   .comment.collapsed .usertext {  \
     display: block;               \
+  }                               \
+  .comment.collapsed .midcol {    \
+    visibility: visible;          \
+    height: auto;                 \
   }                               \
 ")
 
